40b448b25b44a2a113f07c700290cb4fd88e39b8
AnimeBoyYt <abhishekaglawe00@gmail.com>
2022-07-05 11:22:17 +0530

    selection in favorites now working fine

--- src/components/FavoriteContactList.js
+++ src/components/FavoriteContactList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactItem from "./ContactItem";
 import FavoriteListHeader from "./FavoriteListHeader";
 import List from "@material-ui/core/List";
@@ -8,34 +8,58 @@ import NoFavoriteContact from "./NoFavoriteContact";
 function FavoriteContactList(props) {
   const contactList = props.contactList;
   const setContactList = props.setContactList;
+  const [selectionMode, setSelectionMode] = useState(false);
   let noFavsFound = true;
   return (
     <List>
       <FavoriteListHeader
         contactList={contactList}
         setContactList={setContactList}
+        selectionMode={selectionMode}
+        setSelectionMode={setSelectionMode}
       />
       <Divider />
       <div className="contact-list">
-        {contactList.map((contact) => {
-          if (contact.favorite) {
-            noFavsFound=false;
-            return (
-              <ContactItem
-              key={contact.id}
-              id={contact.id}
-              name={contact.firstName + " " + contact.lastName}
-              phone={contact.phone}
-              email={contact.email}
-              favorite={contact.favorite}
-              contactList={contactList}
-              setContactList={setContactList}
-              />
+        {selectionMode
+          ? contactList.map((contact) => {
+            if (contact.favorite) {
+              noFavsFound = false;
+              return (
+                <ContactItem
+                  key={contact.id}
+                  id={contact.id}
+                  name={contact.firstName + " " + contact.lastName}
+                  phone={contact.phone}
+                  email={contact.email}
+                  favorite={contact.favorite}
+                  selected={contact.selected}
+                  selectionMode={selectionMode}
+                  contactList={contactList}
+                  setContactList={setContactList}
+                />
               );
             }
             return false;
-          })}
-          {noFavsFound ? <NoFavoriteContact /> : null}
+            })
+          : contactList.map((contact) => {
+              if (contact.favorite) {
+                noFavsFound = false;
+                return (
+                  <ContactItem
+                    key={contact.id}
+                    id={contact.id}
+                    name={contact.firstName + " " + contact.lastName}
+                    phone={contact.phone}
+                    email={contact.email}
+                    favorite={contact.favorite}
+                    contactList={contactList}
+                    setContactList={setContactList}
+                  />
+                );
+              }
+              return false;
+            })}
+        {noFavsFound ? <NoFavoriteContact /> : null}
       </div>
     </List>
   );
--- src/components/FavoriteListHeader.js
+++ src/components/FavoriteListHeader.js
@@ -8,11 +8,15 @@ import IconButton from "@material-ui/core/IconButton";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 
 function FavoriteListHeader(props) {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [sorted, setSorted] = useState(true);
   const contactList = props.contactList;
   const setContactList = props.setContactList;
+  const selectionMode = props.selectionMode;
+  const setSelectionMode = props.setSelectionMode;
 
   useEffect(() => {
     localStorage.setItem("contactList", JSON.stringify(contactList));
@@ -25,16 +29,52 @@ function FavoriteListHeader(props) {
   const handleMenuClose = () => {
     setAnchorEl(null);
   };
-  const UnfavoriteAllContacts = () =>{
-    const newContactList = contactList.map((contact)=>{
-        if(contact.favorite){
-            contact.favorite=false;
-        }
-        return contact;
-    })
+  const unfavoriteAllContacts = () => {
+    const newContactList = contactList.map((contact) => {
+      if (contact.favorite) {
+        contact.favorite = false;
+      }
+      return contact;
+    });
     setContactList(newContactList);
     setAnchorEl(null);
+  };
+  const unfavoriteSelectedContacts = ()=>{
+    const newContactList = contactList.map((contact)=>{
+      if(contact.selected){
+        contact.favorite=false;
+      }
+      return contact;
+    })
+    setContactList(newContactList);
   }
+  const unSelectAll = () => {
+    const newContactList = contactList.map((contact) => {
+      if (contact.selected) {
+        contact.selected = false;
+      }
+      return contact;
+    });
+    setContactList(newContactList);
+  };
+  const sortContacts = () => {
+    const newContactList = contactList.sort((a, b) => {
+      if (sorted) {
+        if (a.firstName > b.firstName) {
+          return 1;
+        } else {
+          return -1;
+        }
+      } else {
+        if (a.firstName < b.firstName) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    });
+    setContactList(newContactList);
+  };
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
@@ -44,14 +84,34 @@ function FavoriteListHeader(props) {
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      <MenuItem onClick={UnfavoriteAllContacts}>Unfavorite All</MenuItem>
+      {selectionMode ? (
+        <MenuItem onClick={unfavoriteSelectedContacts}>
+          Unfavorite Selected
+        </MenuItem>
+      ) : (
+        <MenuItem onClick={unfavoriteAllContacts}>Unfavorite All</MenuItem>
+      )}
     </Menu>
   );
 
   return (
     <ListItem className="list_header">
       <ListItemIcon>
-        <CheckBoxOutlinedIcon />
+        {selectionMode ? (
+          <CheckBoxOutlinedIcon
+            onClick={() => {
+              setSelectionMode(false);
+              unSelectAll();
+            }}
+          />
+        ) : (
+          <CheckBoxOutlineBlankIcon
+            onClick={() => {
+              setSelectionMode(true);
+              unSelectAll();
+            }}
+          />
+        )}
       </ListItemIcon>
       <div className="name-div">
         <ListItemText primary="Name" />
@@ -63,7 +123,18 @@ function FavoriteListHeader(props) {
         <ListItemText primary="Email" />
       </div>
       <div className="itembuttons-div">
-        <IconButton>
+      <IconButton
+          onClick={() => {
+            sortContacts();
+            setSelectionMode(false);
+            unSelectAll();
+            if (sorted) {
+              setSorted(false);
+            } else {
+              setSorted(true);
+            }
+          }}
+        >
           <SortIcon />
         </IconButton>
         <IconButton aria-haspopup="true" onClick={handleMenuOpen}>
